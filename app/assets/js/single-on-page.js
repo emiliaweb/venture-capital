c7914b0a29cd2a54dbe1a96df18e486fe728d855
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-02-10 18:13:13 +0600

    Create news section

---
+++ app/assets/js/single-on-page.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function resizeIframe(obj) {
+        obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+    }
+
+    const news = document.querySelector('[data-news-container]');
+    const newsIframe = document.querySelector('.news-single iframe');
+
+    news.addEventListener('click', e => {
+        e.preventDefault();
+        if (e.target.classList.contains('news-item')) {
+            const link = e.target.href;
+            console.log(link);
+            newsIframe.src = link;
+            resizeIframe(newsIframe);
+            setTimeout(() => {
+                newsIframe.scrollIntoView();
+                newsIframe.contentWindow.addEventListener('click', e => {
+                    if (e.target.matches('[data-close-post]')) {
+                        newsIframe.style.height = 0;
+                        setTimeout(() => {
+                            newsIframe.src = null;
+                        }, 300)
+                    }
+                })
+            }, 300);
+        }
+    });
+
+})
